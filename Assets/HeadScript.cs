6a1beed97acafd59c2f4790d1f6755244dfb6bcb
leigh <lagebergman@gmail.com>
2017-02-15 16:45:18 +0100

    Add three more feet

---
+++ Assets/HeadScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadScript : MonoBehaviour {
+
+	public Rigidbody head;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float pushHorizontal = Input.GetAxis ("Horizontal");
+		head.AddForce (new Vector3 (0, 0, pushHorizontal * 20));
+
+		float pushVertical = Input.GetAxis ("Vertical");
+		head.AddForce (new Vector3 (0, pushVertical * 50, 0));
+		
+	}
+}
--- Assets/LegScript.cs
+++ Assets/LegScript.cs
@@ -11,6 +11,7 @@ public class LegScript : MonoBehaviour {
 	public Rigidbody knee;
 	public Rigidbody shin;
 	public Rigidbody ankle;
+	public Rigidbody foot;
 	public Rigidbody toe;
 
 	public Transform parentObject;
@@ -23,9 +24,10 @@ public class LegScript : MonoBehaviour {
 	private Vector3 vectorZ;
 
 	private float g;
-	private float m1;
-	private float m2;
-	private float m3;
+	private float mh;
+	private float mt;
+	private float ms;
+	private float mf;
 
 	// Use this for initialization
 	void Start () {
@@ -36,10 +38,13 @@ public class LegScript : MonoBehaviour {
 		parentRotX = parentObject.rotation.eulerAngles.x;
 		parentRotY = parentObject.rotation.eulerAngles.y;
 		parentRotZ = parentObject.rotation.eulerAngles.z;
+
 		g = Physics.gravity.y;
-		m1 = head.mass;
-		m2 = head.mass + thigh.mass;
-		m3 = head.mass + thigh.mass + shin.mass;
+
+		mh = head.mass / 4;
+		mt = thigh.mass;
+		ms = shin.mass;
+		mf = foot.mass;
 
 		print ("parentRotX: " + parentRotX + ", parentRotY: " + parentRotY + ", parentRotZ: " + parentRotZ );
 		
@@ -47,90 +52,46 @@ public class LegScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
 		float thighRotX = thigh.rotation.eulerAngles.x - parentRotX;
 		float thighRotY = thigh.rotation.eulerAngles.y - parentRotY;
 		float thighRotZ = thigh.rotation.eulerAngles.z - parentRotZ;
+		if (thighRotX > 180) {
+			thighRotX = thighRotX - 360;
+		}
 
 		float shinRotX = shin.rotation.eulerAngles.x - parentRotX;
 		float shinRotY = shin.rotation.eulerAngles.y - parentRotY;
 		float shinRotZ = shin.rotation.eulerAngles.z - parentRotZ;
-
 		if (shinRotX > 180) {
 			shinRotX = shinRotX - 360;
 		}
-		double alphaInRadians = Math.PI * (thighRotX / 180.0);
-		float cosAlpha = (float) Math.Sin (alphaInRadians);
-		float kneeForce = m1 * g * cosAlpha;
 
-
-		if (thighRotX > 180) {
-			thighRotX = shinRotX - 360;
+		float footRotX = foot.rotation.eulerAngles.x - parentRotX;
+		float footRotY = foot.rotation.eulerAngles.y - parentRotY;
+		float footRotZ = foot.rotation.eulerAngles.z - parentRotZ;
+		if (footRotX > 180) {
+			footRotX = footRotX - 360;
 		}
+
+		//Cos(90 - a) = Sin(a) 
+
+		double alphaInRadians = Math.PI * (thighRotX / 180.0);
+		float sinAlpha = (float) Math.Sin (alphaInRadians);
+		float kneeForce = mh * g * sinAlpha;
+
+
 		double betaInRadians = Math.PI * (shinRotX / 180.0);
-		float cosBeta = (float) Math.Sin (betaInRadians);
-		float ankleForce = m2 * g * cosBeta;
+		float sinBeta = (float) Math.Sin (betaInRadians);
+		float ankleForce = (mh + mt) * g * sinBeta;
 
-		//double gammaInRadians = Math.PI * (shinRotX / 180.0);
-		//float cosGamma = (float) Math.Sin (gammaInRadians);
-		float toeForce = m3 * g;
 
-		print ("kneeForce = " + kneeForce + ", ankleForce = " + ankleForce + ", toeForce = " + toeForce);
-
+		double gammaInRadians = Math.PI * (footRotX / 180.0);
+		float sinGamma = (float) Math.Sin (gammaInRadians);
+		float toeForce = (mh + mt + ms) * g;
 
 		knee.AddRelativeForce (new Vector3 (0, 0, kneeForce));
 		ankle.AddRelativeForce (new Vector3 (0, 0, -ankleForce));
 		toe.AddRelativeForce(new Vector3 (0, toeForce, 0));
-
-
-		//ankle.AddRelativeForce (new Vector3 (0, 0, 100));
-
-
-		/*
-		//float intensity = Input.GetAxis("Vertical");
-		//thisFoot.AddTorque (vector * tq * intensity);
-		float shinRotX = shin.rotation.x; //.eulerAngles[0]/360;
-		float shinRotY = shin.rotation.y; //.eulerAngles[1]/360;
-		float shinRotZ = shin.rotation.z; //.eulerAngles[2]/360;
-
-		//print (name);
-		if (name == ("RightBackFoot (3)")){
-			print("headless shinRotX = " + shinRotX);
-			print("headless shinRotY = " + shinRotY);
-			print("headless shinRotZ = " + shinRotZ);
-		}
-		int limitAngle = 15;
-		float rotConst = 100f;
-
-		if (shinRotX < (-limitAngle)) {
-			if (name == ("RightBackFoot (3)")){
-				print("headless X thisFoot.AddTorque " + (-(rotConst *  vectorX * tq)));
-			}
-			thisFoot.AddTorque (-(rotConst *  vectorX * tq), ForceMode.Impulse);
-		} else if (shinRotX > limitAngle) {
-			if (name == ("RightBackFoot (3)")){
-				print("headless X thisFoot.AddTorque " + ((rotConst *  vectorX * tq)));
-			}
-			thisFoot.AddTorque (-(rotConst *  vectorX * tq), ForceMode.Impulse);
-
-		}
-
-		//		if (shinRotY < (-limitAngle)) {
-		//			thisFoot.AddTorque (rotConst * shinRotY * vector * tq);
-		//		} else if (shinRotY > limitAngle) {
-		//			thisFoot.AddTorque (-(rotConst * shinRotY * vector * tq));
-		//		}
-
-		if (shinRotZ < (-limitAngle)) {
-			if (name == ("RightBackFoot (3)")){
-				print("headless Z thisFoot.AddTorque " + (-(rotConst *  vectorZ * tq)));
-			}
-			thisFoot.AddTorque (-(rotConst *  vectorZ * tq), ForceMode.Impulse);
-		} else if (shinRotZ > limitAngle) {
-			if (name == ("RightBackFoot (3)")){
-				print("headless Z thisFoot.AddTorque " + (-(rotConst *  vectorZ * tq)));
-			}
-			thisFoot.AddTorque (-(rotConst *  vectorZ * tq), ForceMode.Impulse);
-		}
-		*/
 	}
 }
