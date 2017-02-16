07b0e459b9dfc6c1433cac8315e21b5a65f53264
ThunderSkillz <danieljohansson_95@hotmail.com>
2017-02-16 16:41:35 +0100

    Added some muscle points

--- Assets/MuscleBehaviour.cs
+++ Assets/MuscleBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MuscleBehaviour : MonoBehaviour {
-    public Rigidbody rightUpperHamstring, rightLowerHamstring;
+    public Rigidbody rightFrontUpperHamstring, rightFrontLowerHamstring;
     public float force;
 	// Use this for initialization
 	void Start () {
@@ -12,15 +12,24 @@ public class MuscleBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        ContractRightHamstring(force);
+        ExtendRightFrontHamstring(force);
     }
-    public void ContractRightHamstring(float force)
+    public void ContractRightFrontHamstring(float force)
     {
-        Vector3 direction1 = rightUpperHamstring.transform.position - rightLowerHamstring.transform.position;
+        Vector3 direction1 = rightFrontUpperHamstring.transform.position - rightFrontLowerHamstring.transform.position;
         direction1.Normalize();
         direction1 = direction1 * force;
         Vector3 direction2 = -direction1;
-        rightUpperHamstring.AddForce(direction2*Time.deltaTime);
-        rightLowerHamstring.AddForce(direction1*Time.deltaTime);
+        rightFrontUpperHamstring.AddForce(direction2*Time.deltaTime);
+        rightFrontLowerHamstring.AddForce(direction1*Time.deltaTime);
+    }
+    public void ExtendRightFrontHamstring(float force)
+    {
+        Vector3 direction1 = rightFrontUpperHamstring.transform.position - rightFrontLowerHamstring.transform.position;
+        direction1.Normalize();
+        direction1 = direction1 * force;
+        Vector3 direction2 = -direction1;
+        rightFrontUpperHamstring.AddForce(direction1 * Time.deltaTime);
+        rightFrontLowerHamstring.AddForce(direction2 * Time.deltaTime);
     }
 }
